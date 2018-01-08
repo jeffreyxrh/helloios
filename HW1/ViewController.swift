7a2fa3a1294e2c7d4ee00ca99f2cb29000e02e7e
Ranhao Xu <jeff@Ranhaos-MacBook-Pro.local>
2018-01-08 00:26:33 -0800

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Ranhao Xu on 1/8/18.
+//  Copyright © 2018 Ranhao Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
